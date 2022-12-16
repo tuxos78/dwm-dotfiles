9c09be1a73bc8d84772d8412d0b610ec7dc88457
Tux <62457015+tuxos78@users.noreply.github.com>
2022-12-16 09:46:06 +0300

    Update sbar.sh

--- dwm-conf/dwm/sbar.sh
+++ dwm-conf/dwm/sbar.sh
@@ -3,10 +3,10 @@ while true; do
      
     xsetroot -name " 
 	| $( xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print " RU "; else print " EN "; }' ) 
-	| $( ~/dwm/.scripts/inet.sh ) 
-	|  $( ~/dwm/.scripts/vol.sh ) 
+	| $( ~/dwm/scripts/inet.sh ) 
+	|  $( ~/dwm/scripts/vol.sh ) 
 	|  $(cat /sys/class/backlight/amdgpu_bl0/brightness)% 
-	| $( ~/dwm/.scripts/battery.sh ) 
+	| $( ~/dwm/scripts/battery.sh ) 
 	| $( date +"%T" ) | "
     sleep 1s
     	
