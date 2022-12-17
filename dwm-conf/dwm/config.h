2a5e72d57972448de26bf25274d7a49ec01b42a8
Tux <62457015+tuxos78@users.noreply.github.com>
2022-12-17 08:13:17 +0300

    Update config.h

--- dwm-conf/dwm/config.h
+++ dwm-conf/dwm/config.h
@@ -42,7 +42,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
