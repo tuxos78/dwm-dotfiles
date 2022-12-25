faee17b0833c0c532d8af776edbe656418d77fee
Tux <62457015+tuxos78@users.noreply.github.com>
2022-12-25 17:39:25 +0300

    Delete dwm-conf/dwm directory

--- dwm-conf/dwm/README.md
+++
@@ -1,2 +0,0 @@
-За основу взята сборка dwm от https://github.com/LukeSmithxyz/dwm
-немного изменил под себя.
--- dwm-conf/dwm/scripts/get_vol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
- 
-if [[ $(pactl list sinks | grep -m 1 "Звук выключен:" | cut -d " " -f3) == "yes" ]]; then
-    echo 0%
-else
-    echo $(pactl list sinks | grep -m 1 "Громкость:" | tr -s ' ' | cut -d " " -f5)
-fi
