630ef2e29bc18a9356aa2a043c8ba27ae80412c0
Tux <62457015+tuxos78@users.noreply.github.com>
2022-12-28 12:33:06 +0300

    Delete dwm-conf/dwm directory

--- dwm-conf/dwm/sbar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-while true; do     
-xsetroot -name " | $( ~/dwm/scripts/disk.sh )|  $( xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print " RU "; else print " EN "; }' ) | $( ~/dwm/scripts/inet.sh ) |  $( ~/dwm/scripts/vol.sh ) |  $(cat /sys/class/backlight/amdgpu_bl0/brightness)% | $( ~/dwm/scripts/battery.sh ) | $( date +"%T" ) | "
-sleep 1s
-    	
-  done 
