c5ecf24b76b7810a28224aeabb516702b645a0c8
Tux <62457015+tuxos78@users.noreply.github.com>
2023-04-12 06:29:38 +0300

    Update config.h

--- dwm-conf/st/config.h
+++ dwm-conf/st/config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
-static int borderpx = 3;
+static int borderpx = 6;
 
 /*
  * What program is execed by st depends of these precedence rules:
